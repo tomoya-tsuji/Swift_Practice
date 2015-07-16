44dc463e7b4d9bc9bd74e19ba0c643e4b5c97840
Tomoya Tsuji <1020tomoya@gmail.com>
2015-07-16 16:04:14 +0900

    initial commit

---
+++ ViewSegueTest/ViewSegueTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewSegueTest
+//
+//  Created by TsujiTomoya on 2015/07/16.
+//  Copyright (c) 2015年 astrotom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

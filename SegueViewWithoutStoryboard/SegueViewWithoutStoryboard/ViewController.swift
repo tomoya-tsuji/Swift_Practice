2722bf4a27f87b9b72a94a2b0e5c9e6c58f399a6
Tomoya Tsuji <1020tomoya@gmail.com>
2015-07-16 16:51:16 +0900

    inital commit

---
+++ SegueViewWithoutStoryboard/SegueViewWithoutStoryboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SegueViewWithoutStoryboard
+//
+//  Created by TsujiTomoya on 2015/07/16.
+//  Copyright (c) 2015年 astrotom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

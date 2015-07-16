1780fe7e80ee9dfa66f6de4e1c8b03432c123e6a
Tomoya Tsuji <1020tomoya@gmail.com>
2015-07-16 16:59:32 +0900

    1

--- SegueViewWithoutStoryboard/SegueViewWithoutStoryboard/ViewController.swift
+++ SegueViewWithoutStoryboard/SegueViewWithoutStoryboard/ViewController.swift
@@ -13,13 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
+        let button = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+        button.frame = CGRect(x: 0,y: 0, width: 50, height: 44)
+        button.setTitle("開く", forState: UIControlState.Normal)
+        button.addTarget(
+            self,
+            action: "respondToButtonClick",
+            forControlEvents: UIControlEvents.TouchUpInside
+        )
+        view.addSubview(button);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - UIButton
+    func respondToButtonClick(sender: UIButton){
+    
     }
 
 

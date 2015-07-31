eecf252ad34e758d91947bb5e7d03814217e067b
Tomoya Tsuji <1020tomoya@gmail.com>
2015-07-31 18:24:41 +0900

    update collectionView

--- swiftCollectionViewTest/swiftCollectionViewTest/ViewController.swift
+++ swiftCollectionViewTest/swiftCollectionViewTest/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //コレクションビューに表示するsectionの数を返す処理
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1;
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //sectionに表示するItemの数を返す処理
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1;
     }
-
+    
+    //セルの表示内容をセットする
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath)
+        as! UICollectionViewCell
+        
+        let image = UIImage(named: "mario")
+        let imageView = UIImageView(image: image) as UIImageView
+        let radian = CGFloat(M_PI / 180)* CGFloat(30* indexPath.row)
+        imageView.transform = CGAffineTransformMakeRotation(radian)
+        cell.addSubview(imageView)
+        return cell
+    }
+    
+    //sectionのヘッダーを表示するためのメソッド
+    override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        if kind! = UICollectionElementKindSectionHeader{
+            return super.collectionView(collectionView, viewForSupplementaryElementOfKind: kind, atIndexPath: indexPath)
+        }
+        
+        
+    }
+    
+    
 
 }
 

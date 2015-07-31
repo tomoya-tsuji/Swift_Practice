a71ff9d7f15f2df88d22c68cb64e84195dc4e72d
Tomoya Tsuji <1020tomoya@gmail.com>
2015-07-31 18:49:00 +0900

    update tableViewController

--- SwiftUINavigationControllerClass/SwiftUINavigationControllerClass/ViewController.swift
+++ SwiftUINavigationControllerClass/SwiftUINavigationControllerClass/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let groupNames = ["ビートルズ","スーパーマリオ"]
+    let groups = [
+        ["ジョンレノン","リンゴスター","ジョージハリスン","ポールマッカートニー"],
+        ["マリオ","ルイージ","ピーチ","クッパ","キノピオ"]
+    ]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return self.groupNames.count
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.groups[section].count
     }
-
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel!.text = self.groups[indexPath.section][indexPath.row]
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        var view = tableView.dequeueReusableHeaderFooterViewWithIdentifier("Header")
+          as? UITableViewHeaderFooterView
+        if view == nil{
+            view = UITableViewHeaderFooterView(reuseIdentifier: "Header")
+        }
+        view!.textLabel.text = self.groupNames[section]
+        return view!
+    }
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
 
 }
 
